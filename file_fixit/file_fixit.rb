7fc1ab81d8e847becc456a0adae32051da82d512
Brendan Benson <brendanbenson@gmail.com>
2011-12-20 02:47:24 -0500

    File Fix-it (Round 1B 2010) Solved

--- file_fixit/file_fixit.rb
+++ file_fixit/file_fixit.rb
@@ -1,15 +1,51 @@
-f = File.open('A-small-practice.in')
+f = File.open('A-large-practice.in')
 
-o = File.new('A-small-practice.out', 'w')
+o = File.new('A-large-practice.out', 'w')
 
 cases = f.readline.to_i
 
 cases.times do |i|
   
-  # initialize the values
+  #puts "Case \##{i}\n"
   
-  # execute the code
+  # parse the input and create the data structures
+  params = f.readline.split " "
+  existing = params[0].to_i
+  to_create = params[1].to_i
+  dirs = {}
+  total = 0
   
+  # process the data
+  existing.times do |t|
+    path = f.readline.chomp
+    folders = path.split "/"
+    folders = folders.drop 1
+    while !folders.empty?
+      dirs[path] = true
+      folder = folders.pop.to_s
+      path = path[0..-(folder.length + 2)]
+    end
+  end
+  
+  to_create.times do |t|
+    path = f.readline.chomp
+    folders = path.split "/"
+    folders = folders.drop 1
+    while !folders.empty?
+      if !dirs.has_key? path
+        total = total + 1
+      end
+      dirs[path] = true
+      folder = folders.pop.to_s
+      path = path[0..-(folder.length + 2)]
+    end
+  end
+  
+  #o.syswrite dirs.to_s
+  #o.syswrite "\n"
+  
+  # output the result
+  message = total
   o.syswrite "Case \##{i + 1}: #{message}\n"
   
 end
--- rank_pure/rank_pure.rb
+++ rank_pure/rank_pure.rb
@@ -1,15 +1,17 @@
-f = File.open('C-small-practice.in')
+f = File.open('A-small-practice.in')
 
-o = File.new('C-small-practice.out', 'w')
+o = File.new('A-small-practice.out', 'w')
 
 cases = f.readline.to_i
 
 cases.times do |i|
   
-  # initialize the values
+  # parse the input and create the data structures
   
-  # execute the code
+  # process the data
   
+  # output the result
+  message = ""
   o.syswrite "Case \##{i + 1}: #{message}\n"
   
 end
